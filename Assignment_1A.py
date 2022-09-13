cbdf58b58f3fde59eb8cabf20e5a85d1e67b10d2
Parsa <p.beigzadeh@students.uu.nl>
2022-09-14 00:31:45 +0200

    read and prepare dataset

--- Assignment_1A.py
+++ Assignment_1A.py
@@ -1,5 +1,25 @@
+
+# load libraries 
 import numpy as np
 import pandas as pd
+import sklearn 
 
-data = np.loadtxt('Data/dialog_acts.dat', unpack = True)
-print(data)
+#read dataset as dataframe
+df = pd.read_table("Data/dialog_acts.dat",index_col=False,names=["words"])
+label=[]
+text=[]
+#choose the column
+line_list=df.iloc[:,0]
+#create new dataframe to create new dataset for training 
+dt=pd.DataFrame(columns=['dialogue','uttr'])
+#extract the first word from string
+for line in line_list:
+    first_word= line.split()[0]
+    uttr= line.replace("{} ".format(first_word),'') 
+    label.append(first_word.lower())
+    text.append(uttr.lower())
+#add dialogue and utterance to new dataframe
+dt['dialogue']=label
+dt['uttr']=text
+print(dt)
+
