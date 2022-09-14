35035a1879c546c18a207739244a7fd5d33880d6
Parsa <p.beigzadeh@students.uu.nl>
2022-09-14 12:09:09 +0200

    define lr model

--- Assignment_1A.py
+++ Assignment_1A.py
@@ -3,6 +3,9 @@
 import numpy as np
 import pandas as pd
 import sklearn 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 
 #read dataset as dataframe
 df = pd.read_table("Data/dialog_acts.dat",index_col=False,names=["words"])
@@ -21,5 +24,15 @@ for line in line_list:
 #add dialogue and utterance to new dataframe
 dt['dialogue']=label
 dt['uttr']=text
-print(dt)
+
+#prepare trainset and testset 
+x_train, x_test, y_train, y_test = train_test_split(dt['dialogue'], dt['uttr'], test_size=0.15, random_state=0)
+#define model
+lr = LogisticRegression()
+#fit the model
+lr.fit(x_train, y_train)
+#make a prediction
+predictions = lr.predict(x_test)
+score = lr.score(x_test, y_test)
+print(score)
 
