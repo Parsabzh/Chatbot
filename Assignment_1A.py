5f56bfd258f1906c2342500aa99e7692b206e4ce
jorisvane <70818648+jorisvane@users.noreply.github.com>
2022-09-15 12:01:19 +0200

    Test

--- Assignment_1A.py
+++ Assignment_1A.py
@@ -9,6 +9,7 @@ from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 
 #read dataset as dataframe
 df = pd.read_table("Data/dialog_acts.dat",index_col=False,names=["words"])
+
 label=[]
 text=[]
 #choose the column
@@ -18,7 +19,7 @@ dt=pd.DataFrame(columns=['dialogue','uttr'])
 #extract the first word from string
 for line in line_list:
     first_word= line.split()[0]
-    uttr= line.replace("{} ".format(first_word),'') 
+    uttr= line.replace("{} ".format(first_word),'')
     label.append(first_word.lower())
     text.append(uttr.lower())
 #add dialogue and utterance to new dataframe
@@ -26,7 +27,8 @@ dt['dialogue']=label
 dt['uttr']=text
 
 #prepare trainset and testset 
-x_train, x_test, y_train, y_test = train_test_split(dt['dialogue'], dt['uttr'], test_size=0.15, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(dt['uttr'], dt['dialogue'], test_size=0.15, random_state=0)
+
 #define model
 lr = LogisticRegression()
 #fit the model
@@ -34,5 +36,9 @@ lr.fit(x_train, y_train)
 #make a prediction
 predictions = lr.predict(x_test)
 score = lr.score(x_test, y_test)
-print(score)
+#print(score)
 
+vectorizer = CountVectorizer()
+X_train = vectorizer.fit_transform(x_train)
+X_test = vectorizer.fit_transform(x_test)
+print(np.shape(X_train))
