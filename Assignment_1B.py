dd8b1473dcb882c5c7c813fbbfe76a9967f0a8cf
jorisvane <70818648+jorisvane@users.noreply.github.com>
2022-09-29 15:22:08 +0200

    Added after suggestion state

--- Assignment_1B.py
+++ Assignment_1B.py
@@ -26,35 +26,54 @@ class DialogManager:
 
         speech_act = self.nn.predict(utterance)
 
-        # ONLY WHEN USER INPUT UTTERANCE IS INFORM OR REQUEST START COLLECTING PREFERENCES
-
+        # when user input is inform extract new preferences and suggest restaurant
         if speech_act == 'inform' or speech_act == 'request':
             self.extract_preferences(utterance)
 
             if self.preferences['area'] != '' and self.preferences['food'] != '' and self.preferences[
                 'pricerange'] != '':
+
+                # When the preferences are know go to state suggest restaurant, it can be found or not found
                 state = "suggest_restaurant"
-                self.restaurant = restaurant_suggestion(self.preferences)
-                rst = self.restaurant
-                dialogue_act = "is " + str(rst['restaurantname']) + 'on the' + str(
-                    rst['area']) + ' part of town' + " ok?"
+                scores, self.restaurant = restaurant_suggestion(self.preferences)
+
+                # We look at the distance score if its not zero suggest next best restaurant
+                if scores[0] != 0:
+                    dialogue_act = "Im sorry there is no " + self.preferences[
+                    'pricerange'] + self.preferences['food'] + 'restaurant in ' + self.preferences['area'] + '.'
+                    rst = self.restaurant
+                    dialogue_act = "But we have an alternative. Is " + str(rst['restaurantname']) + 'on the' + str(
+                    rst['area']) + ' part of town with a '+ rst['pricerange'] + " price range ok?"
+                    self.state = 'after_suggestion'
+                else:
+
+                    # Give perfect match
+                    dialogue_act = "Is " + str(rst['restaurantname']) + 'on the' + str(
+                    rst['area']) + ' part of town with a '+ rst['pricerange'] + " price range ok?"
+                    self.state = 'after_suggestion'
             else:
+
+                # When some of the preferences are empty fill them in by going to the request state
                 for key, value in self.preferences.items():
                     if value == '':
                         state = 'request_' + str(key)
                         break
-
+            
+            # Request states to ask about unknown preferences
             if state == 'request_area':
                 dialogue_act = 'In which area would you like to eat?'
-
             if state == 'request_food':
                 dialogue_act = 'What kind of food would you like to eat?'
-
             if state == 'request_pricerange':
                 dialogue_act = 'What pricerange does the food have to be?'
 
-        # WHEN USER INPUT STATES A GOODBYE UTTERANCE GO TO END STATE
+        # After restaurant suggestion affirm, deny and request dialogue acts
+        if state == 'after_suggestion':
+            if speech_act == 'affirm':
+                self.state = 'end'
+                dialogue_act = "Thank you for using the system. Goodbye!"
 
+        # After goodbye utterance go to end state
         if speech_act == 'goodbye':
             self.state = 'end'
             dialogue_act = "Thank you for using the system. Goodbye!"
@@ -113,7 +132,7 @@ def restaurant_suggestion(preferences):
             distance = ls.distance(value, restaurant[preference])
             score += distance
         scores.append((score, restaurant))
-    return min(scores, key=lambda x: x[0])[1]  # return highest scoring restaurant
+    return scores, min(scores, key=lambda x: x[0])[1]  # return highest scoring restaurant
 
 
 print(restaurant_suggestion({'pricerange': 'expenove', 'food': 'spenush'}))
